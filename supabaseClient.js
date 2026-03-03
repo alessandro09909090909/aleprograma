b8bed6bb8f81dad0554f3a307e6267b7a8eccac9
alessandro09909090909 <alessandro.santos.cardoso@escola.pr.gov.br>
2026-03-03 09:37:22 -0300

    ok

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,9 @@
-import { createClient } from 'https://senlgkvwljqfrduvncov.supabase.co'
+// Importa a função de criação do client Supabase via CDN
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
- export  const supabaseKey = createClient ('anon public:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlbmxna3Z3bGpxZnJkdXZuY292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTQwOTEsImV4cCI6MjA4NzY5MDA5MX0.skGnoVlZsuDSVqQI7EM-mRFRWyn-8lBYbqIJRxslqPs'
- )
+// Cria o client com a URL e a chave fornecidas
+export const supabase = createClient(
+  'https://senlgkvwljqfrduvncov.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlbmxna3Z3bGpxZnJkdXZuY292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTQwOTEsImV4cCI6MjA4NzY5MDA5MX0.skGnoVlZsuDSVqQI7EM-mRFRWyn-8lBYbqIJRxslqPs'
+
+)
