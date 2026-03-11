69a74ea0922a95b40a93657530e33becf2f8f571
alessandro09909090909 <alessandro.santos.cardoso@escola.pr.gov.br>
2026-03-11 16:04:47 -0300

    ok

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 // Importa a função de criação do client Supabase via CDN
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// Cria o client com a URL e a chave fornecidas
+// Cria o cliente com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://senlgkvwljqfrduvncov.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlbmxna3Z3bGpxZnJkdXZuY292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTQwOTEsImV4cCI6MjA4NzY5MDA5MX0.skGnoVlZsuDSVqQI7EM-mRFRWyn-8lBYbqIJRxslqPs'
